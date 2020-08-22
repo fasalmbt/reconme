474e773a2c7a9bd011fda935178cbf12bf05e958
fasalmbt <>
2020-08-22 19:08:21 +0530

    Banner updated,requirements updated - V1.2.2

--- core/banners.py
+++ core/banners.py
@@ -1,7 +1,11 @@
-#banners.py
-
-from termcolor import colored
-from pyfiglet import figlet_format
-
 def banner():
-	print((colored(figlet_format("RECONME"), color="green")))
+	green = '\033[32m'
+	print('''%s
+	 __  __  __  __   _     _ _   __
+	|_/ |__ |   |  | | | | | | | |__
+	| \ |__ |__ |__| | |_| |   | |__
+          
+               V 1.2.2
+          credits - @fasalmbt
+
+	\033[0m''' %green)
