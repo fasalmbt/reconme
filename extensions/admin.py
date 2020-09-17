f22c67678e5299aab960cd9a3dbcbad7ae87e3c5
fasalmbt <>
2020-09-17 20:27:25 +0530

    Updated admin panel list

--- extensions/admin.py
+++ extensions/admin.py
@@ -2,7 +2,8 @@ import urllib.request as r
 import urllib.error as e
 def admin(url):
 	#add your own list
-	links=('/admin','/administrator','/admincp','/apanel','admin/login.php')
+	links=('admin/', 'admin.php', 'administrator/', 'login.php', 'login/', 'login.html', 'admin.html', 'admin/admin.php',
+'admin_login.php', 'admin/account.php', 'admin/login.php', 'login/login.php', 'login/admin.php')
 	for admin_link in links:
 		request = "https://"+url+admin_link
 		try:
