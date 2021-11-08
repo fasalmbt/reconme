0609c42bcbb927ccac59218f1c650cc7084dd167
fasalmbt <>
2021-11-09 00:01:12 +0530

    Updated admin search

--- extensions/admin.py
+++ extensions/admin.py
@@ -1,18 +1,17 @@
 import urllib.request as r
 import urllib.error as e
+import os
+import sys
 def admin(url):
-	#add your own list
-	links=('admin/', 'admin.php', 'administrator/', 'login.php', 'login/', 'login.html', 'admin.html', 'admin/admin.php',
-'admin_login.php', 'admin/account.php', 'admin/login.php', 'login/login.php', 'login/admin.php')
-	for admin_link in links:
-		request = "https://"+url+admin_link
-		try:
-			req = r.urlopen(request)
-			code = req.getcode()
-			if code == 200:
-				print("\n")
-				print("admin panel ==> "+request)
-				break
-		except e.URLError as err:
-			print("\n")
-			print("Sorry not found at ==> "+request)
+	with open(os.path.join(sys.path[0], "extensions/admin.txt")) as link:
+		links = link.readlines()
+		for admin_link in links:
+			request = "https://"+url+"/"+admin_link
+			try:
+				req = r.urlopen(request)
+				code = req.getcode()
+				if code == 200:
+					print("admin panel ==> "+request)
+					break
+			except e.URLError as err:
+				print("Sorry not found at ==> "+request)
